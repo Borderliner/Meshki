837aebd06dd1b84136ea85a47b35e7cc1c27a6a3
Borderliner <hajianpour.mr@gmail.com>
2016-08-04 16:05:04 +0430

    Added uglify to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,20 @@ module.exports = function(grunt){
 				src: 'src/css/*.css',
 				dest: 'dist/meshki.min.css'
 			}
-		}
+		},
+		uglify: {
+	    options: {
+	      mangle: false
+	    },
+	    my_target: {
+	      files: {
+	        'dist/meshki.min.js': ['src/js/meshki.js']
+	      }
+	    }
+	  }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.registerTask('default', ['cssmin']);
 };
