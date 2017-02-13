3a643f934a18349106ebeb99651d3e6ff8ef9b96
Borderliner <hajianpour.mr@gmail.com>
2017-02-14 02:54:28 +0330

    Add meshki.search() for page search

---
+++ docs/finder.js
@@ -0,0 +1,39 @@
+var meshki = {
+  TRange: null,
+  search: function() {
+    var str = document.getElementById('searchbox').value;
+    if (parseInt(navigator.appVersion) < 4)
+      return;
+    var strFound;
+    if (window.find) {
+      strFound = self.find(str);
+      if (!strFound) {
+        strFound = self.find(str,0,1);
+        while (self.find(str,0,1))
+          continue;
+      }
+    }
+    else if (navigator.appName.indexOf("Microsoft") != -1) {
+      if (meshki.TRange != null) {
+        meshki.TRange.collapse(false);
+        strFound = meshki.TRange.findText(str);
+        if (strFound)
+          meshki.TRange.select();
+      }
+      if (meshki.TRange == null || strFound == 0) {
+        meshki.TRange = self.document.body.createTextRange();
+        strFound = meshki.TRange.findText(str);
+        if (strFound)
+          meshki.TRange.select();
+      }
+    }
+    else if (navigator.appName == "Opera") {
+      alert ("Opera browsers not supported, sorry...")
+      return;
+    }
+    if (!strFound)
+      alert ("String '"+str+"' not found!")
+    return;
+  }
+};
+
