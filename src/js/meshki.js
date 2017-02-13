9f9ddc743c25b1f2eea950aa1a4881184478b76d
Borderliner <hajianpour.mr@gmail.com>
2017-02-14 01:58:26 +0330

    Change "content" and "container"'s roles with eachother

--- src/js/meshki.js
+++ src/js/meshki.js
@@ -29,22 +29,22 @@ function is_rtl() {
 var meshki = {
   openNav: function() {
     var sidenav = document.getElementsByClassName('sidenav')[0];
-    var content = document.getElementsByClassName('content')[0];
+    var container = document.getElementsByClassName('container')[0];
     // Is sidenav a "Push Sidenav"?
     var isSidenavPush = (sidenav.className.split(' ').indexOf('push') > -1);
     var overlayDiv = document.getElementsByClassName('overlay')[0];
 
     // Set Sidenav's width to 250px, starts sliding
     sidenav.style.width = '250px';
-    // If on Desktop and the sidenav is a push one, push "content"
+    // If on Desktop and the sidenav is a push one, push "container"
     if (window.innerWidth > 768 && isSidenavPush) {
       // Hide body overflow-x
       document.body.style.overflowX = 'hidden';
       // If not RTL
       if (!is_rtl())
-        content.style.marginLeft = '250px';
+        container.style.marginLeft = '250px';
       else
-        content.style.marginRight = '250px';
+        container.style.marginRight = '250px';
     }
 
     overlayDiv.style.opacity = 0.4;
@@ -53,7 +53,7 @@ var meshki = {
 
   closeNav: function() {
     var sidenav = document.getElementsByClassName('sidenav')[0];
-    var content = document.getElementsByClassName('content')[0];
+    var container = document.getElementsByClassName('container')[0];
     var overlayDiv = document.getElementsByClassName('overlay')[0];
     var isSidenavPush = (sidenav.className.split(' ').indexOf('push') > -1);
 
@@ -61,7 +61,7 @@ var meshki = {
     sidenav.style.width = '0';
 
     if (window.innerWidth > 768 && isSidenavPush)
-      content.style.margin = '0';
+      container.style.margin = '0';
 
     overlayDiv.style.opacity = 0;
     overlayDiv.style.visibility = 'hidden';
