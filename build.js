6f8e5b5f4bb9db115687aed44cb6f87ece69bc4a
Borderliner <hajianpour.mr@gmail.com>
2017-06-06 17:05:06 +0430

    Add build.js for manual build management

---
+++ build.js
@@ -0,0 +1,28 @@
+const fs     = require('fs')
+const colors = require('colors')
+
+function remove_dir(path) {
+  if (fs.existsSync(path)) {
+     fs.readdirSync(path).forEach((file, index) => {
+       var current_path = `${path}/${file}`
+       if (fs.lstatSync(current_path).isDirectory()) {
+         remove_dir(current_path);
+       } else {
+         fs.unlinkSync(current_path)
+       }
+     })
+     fs.rmdirSync(path)
+   }
+}
+
+function create_dist() {
+  if(!fs.existsSync('dist/')) {
+    console.log('No "dist" folder exists. Creating folders...'.yellow);
+    fs.mkdirSync('dist/');
+    fs.mkdirSync('dist/plugins/');
+    fs.mkdirSync('dist/fonts/');
+  } else {
+    console.log('Cleaning previously compiled files...'.yellow);
+    remove_dir('dist/');
+  }
+}
