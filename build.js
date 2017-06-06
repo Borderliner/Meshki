a13a1e7cd75fdf6abf8acdc456c7010a6207e770
Borderliner <hajianpour.mr@gmail.com>
2017-06-06 19:15:15 +0430

    Add functions to compile and minify Meshki

--- build.js
+++ build.js
@@ -1,24 +1,36 @@
-const fs     = require('fs')
-const colors = require('colors')
-const sass   = require('node-sass')
+const fs        = require('fs')
+const path      = require('path')
+const colors    = require('colors')
+const sass      = require('node-sass')
+const cleanCSS  = require('clean-css')
 
 const options = {
-  main_scss:  'src/scss/main.scss',
-  output_css: 'dist/css/meshki.css',
-  plugins:    ['extra-button-colors', 'rtl'],
+  main_scss:      'src/scss/main.scss',
+  output_dir:     'dist/',
+  output_css:     'dist/css/meshki.css',
+  output_css_min: 'dist/css/meshki.min.css',
+  plugins_dir:    'src/scss/plugins/',
+  source_map:     false,
 }
 
-function remove_dir(path) {
-  if (fs.existsSync(path)) {
-     fs.readdirSync(path).forEach((file, index) => {
-       var current_path = `${path}/${file}`
+const plugins = list_dir(options.plugins_dir)
+
+function list_dir(srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+}
+
+function remove_dir(srcpath) {
+  if (fs.existsSync(srcpath)) {
+     fs.readdirSync(srcpath).forEach((file, index) => {
+       var current_path = `${srcpath}/${file}`
        if (fs.lstatSync(current_path).isDirectory()) {
          remove_dir(current_path);
        } else {
          fs.unlinkSync(current_path)
        }
      })
-     fs.rmdirSync(path)
+     fs.rmdirSync(srcpath)
    }
 }
 
@@ -40,23 +52,64 @@ function create_dist() {
 
 function sassify_meshki() {
   console.log('Compiling Meshki...'.yellow)
-  sass.render({
-    file:    options.main_scss,
-    outFile: options.output_css,
-  }, (error, result) => {
-    if (!error) {
-      fs.writeFile(options.output_css, result.css, (error) => {
-        if (!error) {
-          console.log('=> Successfully compiled Meshki'.green)
-        } else {
-          console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
-        }
-      })
-    } else {
-      console.log('Could not find the main Scss file.'.red)
+  let result = sass.renderSync({
+    file:        options.main_scss,
+    outFile:     options.output_css,
+  })
+  // Write CSS file
+  try {
+    fs.writeFile(options.output_css, result.css)
+    console.log('=> Successfully compiled Meshki'.green)
+  } catch (error) {
+    console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
+  }
+}
+
+function minify_meshki() {
+  console.log('Minifying Meshki...'.yellow)
+  let result = sass.renderSync({
+    file:        options.main_scss,
+    outputStyle: 'compressed',
+    outFile:     options.output_css_min,
+    sourceMap:   options.source_map,
+  })
+  // Write Minified CSS file
+  try {
+    fs.writeFile(options.output_css_min, result.css)
+    console.log('=> Successfully minified Meshki'.green)
+  } catch (error) {
+    console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
+  }
+
+  // Write SourceMaps
+  try {
+    fs.writeFile(`${options.output_css}.map`, result.map)
+    console.log('=> Successfully generated source maps for Meshki'.green)
+  } catch (error) {
+    console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
+  }
+}
+
+function sassify_plugins() {
+  console.log('Starting to compile plugins...'.blue)
+  plugins.forEach((name, index) => {
+    console.log(`Compiling `.yellow + `${name}`.blue)
+    let result = sass.renderSync({
+      file:        options.plugins_dir + name + '/main.scss',
+      outFile:     options.output_dir + 'plugins/',
+    })
+    // Write CSS file
+    try {
+      fs.writeFileSync(`${options.output_dir}plugins/${name}.css`, result.css)
+      console.log(`=> Successfully compiled ${name}`.green)
+    } catch (error) {
+      console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
+      console.log(error)
     }
   })
 }
 
 create_dist()
 sassify_meshki()
+minify_meshki()
+sassify_plugins()
