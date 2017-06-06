06a46bdb985bfd8c8eac9132c98a03c5719144b1
Borderliner <hajianpour.mr@gmail.com>
2017-06-06 17:34:10 +0430

    Add sassify_meshki() function to compile Meshki's main.scss

--- build.js
+++ build.js
@@ -2,6 +2,12 @@ const fs     = require('fs')
 const colors = require('colors')
 const sass   = require('node-sass')
 
+const options = {
+  main_scss:  'src/scss/main.scss',
+  output_css: 'dist/css/meshki.css',
+  plugins:    ['extra-button-colors', 'rtl'],
+}
+
 function remove_dir(path) {
   if (fs.existsSync(path)) {
      fs.readdirSync(path).forEach((file, index) => {
@@ -24,9 +30,33 @@ function create_dist() {
     fs.mkdirSync('dist/fonts/')
     fs.mkdirSync('dist/css/')
     fs.mkdirSync('dist/js/')
+    console.log('=> "dist" folder was created successfully!'.green)
   } else {
     console.log('Cleaning previously compiled files...'.yellow)
     remove_dir('dist/')
     create_dist()
   }
 }
+
+function sassify_meshki() {
+  console.log('Compiling Meshki...'.yellow)
+  sass.render({
+    file:    options.main_scss,
+    outFile: options.output_css,
+  }, (error, result) => {
+    if (!error) {
+      fs.writeFile(options.output_css, result.css, (error) => {
+        if (!error) {
+          console.log('=> Successfully compiled Meshki'.green)
+        } else {
+          console.log('Could not write the output to the disk. Check if you have write permissions.'.red)
+        }
+      })
+    } else {
+      console.log('Could not find the main Scss file.'.red)
+    }
+  })
+}
+
+create_dist()
+sassify_meshki()
